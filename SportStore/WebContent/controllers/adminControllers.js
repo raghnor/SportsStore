bbdc950204275b3cf66a3419f5c34674b4365ce6
raghnor <raghnor72@yahoo.com>
2014-11-04 15:19:09 +0100

    Working on admin module (Ch8)

--- SportStore/WebContent/controllers/adminControllers.js
+++ SportStore/WebContent/controllers/adminControllers.js
@@ -3,13 +3,32 @@ angular.module('sportsStoreAdmin')
 	.controller('authCtrl', function($scope, $http, $location, authUrl) {
 		
 		$scope.authenticate = function(user, pass) {
-			$http.post(authUrl, {username: user, password: pass}, {withCredentials: true})
+			$http.post(authUrl, {username: user, password: pass})
 				.success(function(data) {
-					$location.path('/main');
+					if(data.result == 'OK')
+						$location.path('/main');
+					else
+						$scope.authenticationError = 'Incorrect username and / or password';
 				})
 				.error(function(error) {
 					$scope.authenticationError = error;
 				});
+		}
+		
+	})
+	.controller('mainCtrl',function($scope) {
+		
+		$scope.screens = ['Products', 'Orders'];
+		$scope.current = $scope.screens[0];
+		
+		$scope.setScreen = function(index) {
+			$scope.current = $scope.screens[index];
+		};
+		
+		$scope.getScreen = function() {
+			return $scope.current == 'Products'
+				? 'views/adminProducts.html'
+						: 'views/adminOrders.html';
 		}
 		
 	});
